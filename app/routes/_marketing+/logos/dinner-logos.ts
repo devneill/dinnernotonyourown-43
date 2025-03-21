cba1b7c58c17d38310c9bbb7969bcaca28fcd542
Devon Neill <35456245+devneill@users.noreply.github.com>
2025-03-21 08:48:14 +0100

    lp

---
+++ app/routes/_marketing+/logos/dinner-logos.ts
@@ -0,0 +1,99 @@
+import restaurant from './dinner/restaurant.svg'
+import fork from './dinner/fork.svg'
+import pizza from './dinner/pizza.svg'
+import utensils from './dinner/utensils.svg'
+import bowl from './dinner/bowl.svg'
+import chef from './dinner/chef.svg'
+import plate from './dinner/plate.svg'
+import hamburger from './dinner/hamburger.svg'
+import wine from './dinner/wine.svg'
+import coffee from './dinner/coffee.svg'
+import iceCream from './dinner/ice-cream.svg'
+import cake from './dinner/cake.svg'
+
+export const dinnerLogos = [
+	{
+		src: restaurant,
+		alt: 'Restaurant',
+		href: '#',
+		column: 1,
+		row: 1,
+	},
+	{
+		src: fork,
+		alt: 'Fork',
+		href: '#',
+		column: 1,
+		row: 2,
+	},
+	{
+		src: pizza,
+		alt: 'Pizza',
+		href: '#',
+		column: 1,
+		row: 3,
+	},
+	{
+		src: utensils,
+		alt: 'Utensils',
+		href: '#',
+		column: 2,
+		row: 2,
+	},
+	{
+		src: bowl,
+		alt: 'Bowl',
+		href: '#',
+		column: 2,
+		row: 3,
+	},
+	{
+		src: chef,
+		alt: 'Chef',
+		href: '#',
+		column: 2,
+		row: 4,
+	},
+	{
+		src: plate,
+		alt: 'Plate',
+		href: '#',
+		column: 2,
+		row: 5,
+	},
+	{
+		src: hamburger,
+		alt: 'Hamburger',
+		href: '#',
+		column: 2,
+		row: 6,
+	},
+	{
+		src: wine,
+		alt: 'Wine',
+		href: '#',
+		column: 3,
+		row: 3,
+	},
+	{
+		src: coffee,
+		alt: 'Coffee',
+		href: '#',
+		column: 3,
+		row: 4,
+	},
+	{
+		src: iceCream,
+		alt: 'Ice Cream',
+		href: '#',
+		column: 3,
+		row: 5,
+	},
+	{
+		src: cake,
+		alt: 'Cake',
+		href: '#',
+		column: 4,
+		row: 1,
+	},
+] 
